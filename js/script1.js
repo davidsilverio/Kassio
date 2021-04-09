585d90334eed366eb90b67704a987bf7bfeee1f0
davidsilverio <contatodavidsil@gmail.com>
2021-04-09 19:49:02 -0300

    Initial commit

---
+++ js/script1.js
@@ -0,0 +1,14 @@
+$(function() {
+	const todoList = {
+		$container: $(".todo-list"),
+		$input : $("input:text"),
+		$btn: $(".todo-list .btn"),
+		
+		init: function() {
+			this.$input.on("keyup", (e) => {
+				
+			});
+		}
+	}
+	
+}
